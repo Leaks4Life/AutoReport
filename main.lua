d7a141fd9465ddd821ff50ba840599371ad5c5ca
Leaks4Life <74506701+Leaks4Life@users.noreply.github.com>
2022-07-14 10:42:04 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- Not leaking since its already popular on like robloxscripts.com mines custom L
+game.Players.LocalPlayer:Kick('L')
+if messagebox then
+  messagebox('Loser','Loseer'
+end
+    
+    
